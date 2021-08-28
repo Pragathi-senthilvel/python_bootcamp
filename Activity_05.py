a022e9be29fad6480e650e76155d2c623a139ad6
Pragathi-senthilvel <89657567+Pragathi-senthilvel@users.noreply.github.com>
2021-08-28 14:32:06 +0530

    Update Activity_05.py

--- Activity_05.py
+++ Activity_05.py
@@ -1,6 +1,6 @@
-print("Enter five numbers:")
-number_string_list = map(int, input().split())
+print("Enter five numbers a,b,c,d,e:")
+a,b,c,d,e = map(int, input().split())
 sum = 0
-for i in number_string_list:
+for i in a,b,c,d,e:
 	sum = sum + int(i)
 print("Sum of all the number is =",sum)
