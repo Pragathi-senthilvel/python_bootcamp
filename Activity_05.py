554cc7a46758ad7f84fc154d4bf5c25fd2ec384c
Pragathi-senthilvel <89657567+Pragathi-senthilvel@users.noreply.github.com>
2021-08-28 17:18:19 +0530

    Update Activity_05.py

--- Activity_05.py
+++ Activity_05.py
@@ -4,3 +4,12 @@ sum = 0
 for i in a,b,c,d,e:
 	sum = sum + int(i)
 print("Sum of all the numbers is =",sum)
+
+
+input_int = input("Enter five numbers a,b,c,d,e : ")
+list  = input_int.split()
+print("Calculating sum of input list")
+sum = 0
+for num in list:
+    sum += int (num)
+print("Sum = ",sum)
