70a262252a19a9a131fe3d43eb18f1b308a135a9
Pragathi-senthilvel <89657567+Pragathi-senthilvel@users.noreply.github.com>
2021-08-28 20:37:08 +0530

    Update Activity_06.py

--- Activity_06.py
+++ Activity_06.py
@@ -6,3 +6,8 @@ a_list[0] = a_list[4]= 0
 print("Replaced list-1 : ",a_list)
 first_three[0] = first_three[2] = 0
 print("Replaced list-2 : ",first_three)
+
+input_int = input("Enter five numbers : ")
+a_list = input_int.split()
+last_three = a_list[3:]
+print("Sliced list is : ", last_three)
