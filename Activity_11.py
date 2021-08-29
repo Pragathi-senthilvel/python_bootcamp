314e93786b48b5c6a7ccd2d52e8cefeb7236ea6b
Pragathi-senthilvel <89657567+Pragathi-senthilvel@users.noreply.github.com>
2021-08-29 12:21:47 +0530

    Update Activity_11.py

--- Activity_11.py
+++ Activity_11.py
@@ -1,6 +1,17 @@
-def main(a,b):
- summation =a+b
- return(a,b,summation)
-num1=int(input("Enter number one: "))
-num2=int(input("Enter number two: "))
-print("Number one, Number two, Sum = ", main(num1,num2))
+def main():
+    a,b=input_numbers()
+    summation=add(a,b)
+    display(a,b,summation)
+    
+def input_numbers():
+    a,b=input("Enter two numbers : ").split()
+    a,b=[int(a),int(b)]
+    return a,b
+
+def add(x,y):
+    return x+y
+
+def display(x,y,sum):
+   print("The sum of {0} + {1} = {2}".format(x,y,sum))
+
+main()
