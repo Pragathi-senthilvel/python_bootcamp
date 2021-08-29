a533b4dd0275e8646ea258576964aadb8b4213fe
Pragathi-senthilvel <89657567+Pragathi-senthilvel@users.noreply.github.com>
2021-08-29 11:09:08 +0530

    Update Activity_09.py

--- Activity_09.py
+++ Activity_09.py
@@ -1,10 +1,10 @@
 l =float(input("Enter length l of the tromboloid : "))
 b =float(input("Enter breadth b of the tromboloid : "))
 h =float(input ("Enter height h of the tromboloid : "))
-k =(l*l)+(b*b)+(h*h)
+k =(l**2)+(b**2)+(h**2)
 squareroot = k ** 0.5
-volumeoftromboloid = ((b*b)*(h*h))/(squareroot)
+volumeoftromboloid = ((b**2)*(h**2))/(squareroot)
 print("Volume of the tromboloid is = %.3f " %volumeoftromboloid)
 squareroot = k ** 0.5
-radiusofsphere = ((3*(b*b)*(h*h))/((4*3.14)*(squareroot)))**1/3
+radiusofsphere = ((3*(b**2)*(h**2))/((4*3.14)*(squareroot)))**1/3
 print("Radius of sphere having volume same as that of tromboloid is = %.3f " %radiusofsphere)
