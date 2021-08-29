2dd8e0eab4a509134467b65c1d88d87c87653057
Pragathi-senthilvel <89657567+Pragathi-senthilvel@users.noreply.github.com>
2021-08-29 22:37:40 +0530

    Update Activity_15.py

--- Activity_15.py
+++ Activity_15.py
@@ -1,22 +1,32 @@
 import math
-def takeinput():                                               
+def inputs():                                               
     l = float(input("Enter the length of tromboloid: "))
     b = float(input("Enter the breadth of tromboloid: "))
     h = float(input("Enter the height of tromboloid: "))
     return l, b, h
 
-def calculate(l,b,h): 
-    k=(l**2)+(b**2)+(h**2)
-    vol=((b**2)*(h**2))/(math.sqrt(k))
-    rad=((3*(b**2)*(h**2))/((4*math.pi)*(math.sqrt(k))))**1/3
-    return vol,rad
+def dimension(l, b, h):                
+    k = (l**2 + b**2 + h**2)
+    return k
 
-def display(vol,rad):
-    print("Volume of the tromboloid is = %.3f " %vol)
-    print("Radius of sphere having volume same as that of tromboloid is = %.3f " %rad)
-    
+def voltromb(b, h, k):                    
+    vol_tromb = ((b**2)*(h**2))/math.sqrt(k)
+    return vol_tromb
+
+def radsphere(vol_tromb):            
+    rad_cube = (3*vol_tromb)/(4*math.pi)
+    rad_sphere = (rad_cube)**(1/3)
+    return rad_sphere
+
+def display(vol_tromb, rad_sphere):                                 
+    print("The volume of the tromboloid is %.3f" %(vol_tromb))
+    print("The radius of the sphere having the same volume of the given tromboloid is %.3f" %(rad_sphere))
+
 def main():
-    l,b,h=takeinput()
-    vol,rad=calculate(l,b,h)
-    display(vol,rad)
+    l, b, h = inputs()
+    k = dimension(l, b, h)
+    vol_tromb = voltromb(b, h, k)
+    rad_sphere = radsphere(vol_tromb)
+    display(vol_tromb, rad_sphere)
+
 main()
