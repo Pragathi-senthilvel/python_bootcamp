6fe4e7021341122e7801ce63918c90776e077483
Pragathi-senthilvel <89657567+Pragathi-senthilvel@users.noreply.github.com>
2021-08-29 11:12:33 +0530

    Update Activity_09.py

--- Activity_09.py
+++ Activity_09.py
@@ -5,6 +5,5 @@ k =(l**2)+(b**2)+(h**2)
 squareroot = k ** 0.5
 volumeoftromboloid = ((b**2)*(h**2))/(squareroot)
 print("Volume of the tromboloid is = %.3f " %volumeoftromboloid)
-squareroot = k ** 0.5
 radiusofsphere = ((3*(b**2)*(h**2))/((4*3.14)*(squareroot)))**1/3
 print("Radius of sphere having volume same as that of tromboloid is = %.3f " %radiusofsphere)
