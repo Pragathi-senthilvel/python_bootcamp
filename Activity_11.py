5e28598f446d1f55f835ef28e601ddfd285bfa02
Pragathi-senthilvel <89657567+Pragathi-senthilvel@users.noreply.github.com>
2021-08-29 11:49:07 +0530

    Update Activity_11.py

---
+++ Activity_11.py
@@ -0,0 +1,6 @@
+def main(a,b):
+ summation =a+b
+ return(a,b,summation)
+num1=int(input("Enter number one: "))
+num2=int(input("Enter number two: "))
+print("Number one, Number two, Sum = ", main(num1,num2))
