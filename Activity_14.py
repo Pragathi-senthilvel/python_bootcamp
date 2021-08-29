4d413e35e1f7c3b1df056ac503589ed4cc14424d
Pragathi-senthilvel <89657567+Pragathi-senthilvel@users.noreply.github.com>
2021-08-29 21:16:12 +0530

    Update Activity_14.py

--- Activity_14.py
+++ Activity_14.py
@@ -1,11 +1,24 @@
-def CheckIfPrime ():
-    num1 = input("Enter a number to check weather it is a prime number or not: ")
-    a = int(num1)
-    b = 2
-    c = ("Yes {0} is a prime number".format(num1))
-    while b < a:
-        if a%b == 0:
-            c = ("No {0} is not a prime number".format(num1))
-        b = b+1
-    print(c)
-CheckIfPrime ()
+import math
+def userinput():
+    num = int(input("Enter a number to check weather it is a prime number or composite number: "))
+    return num
+
+def prime(num):
+    isprime = True
+    for x in range(2, int(math.sqrt(num))+1):
+        if(num%x==0):
+            isprime = False
+            break
+    return(isprime)
+
+def display(isprime, num):
+    if(isprime==True):
+        print("%d is a prime number"%(num))
+    else:
+        print("%d is a composite number"%(num))
+
+def main():
+    num = userinput()
+    isprime = prime(num)
+    display(isprime, num)
+main()
