bcc0bfc69d0a8e94affcb3511ca940eae029da56
Pragathi-senthilvel <89657567+Pragathi-senthilvel@users.noreply.github.com>
2021-08-29 14:25:02 +0530

    Update Activity_12.py

--- Activity_12.py
+++ Activity_12.py
@@ -2,7 +2,6 @@ def input_num():
     a,b,c=input("Enter three numbers: ").split()
     a,b,c=[int(a),int(b),int(c)]
     return a,b,c
-   
 def big(a,b,c):
     if (a > b) and (a > c):
         greatest=a
@@ -11,10 +10,9 @@ def big(a,b,c):
     else:
         greatest=c
     return greatest
-   
+
 def main():
     x,y,z=input_num()
     greatest=big(x,y,z)
-    print("{0} is the greatest number among {1},{2} and {3}".format(greatest,x,y,z))
+    print("{0}is the greatest number among {1},{2} and {3}".format(greatest,x,y,z))
 main()
-
